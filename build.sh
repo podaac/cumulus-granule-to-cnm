75689df584898ff7c16f5f8d0c016c0b6b4eb1de
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2022-03-18 16:55:53 -0500

    update pipeline with releasing ability

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ cd package ; zip -r ../artifact.zip . -x '*.pyc'
 cd ..
 #code_version=`python -c 'from cumulus_granule_to_cnm import __version__; print(__version__)'`
 code_version=`poetry version | awk '{print $2}'`
-mv artifact.zip artifact-$code_version.zip
+cp artifact.zip artifact-$code_version.zip
 echo \*\* artifact-$code_version.zip created \*\*
